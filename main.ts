edae50d5a5a7da21c55af24635eca13669b325eb
gavin-gallant <gavin.gallant@mths.ca>
2024-10-01 11:17:09 -0400

    This program plays the game cookie clicker with one upgrade available

--- main.ts
+++ main.ts
@@ -1,8 +1,26 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Gavin Gallant
+ * Created on: oct 2024
+ * This program plays the game cookie clicker with one upgrade avalable 
 */
 
-basic.showString('Hello, World!')
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+let cookieNumber = 0
+let cookieIncrease = 1
+
+input.onButtonPressed(Button.A, function (){
+    cookieNumber = cookieNumber + cookieIncrease
+    basic.showNumber(cookieNumber)
+})
+
+input.onButtonPressed(Button.B, function () {
+    if (cookieNumber > 14) {
+        cookieNumber = cookieNumber - 15;
+        cookieIncrease = cookieIncrease + 1
+    }
+    else {
+        basic.showString("to poor")
+    }
+})
