c4760c712faabaf03b172b99246bf639cbf185dc
gavin-gallant <gavin.gallant@mths.ca>
2024-10-02 15:20:44 +0000

    fixed code added : and removed spaces

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -7,4 +7,20 @@ This module is a Micro:bit MicroPython program
 from microbit import *
 
 
-display.scroll("Hello, World!")
+display.clear
+display.show(Image.HAPPY)
+cookie_increase = 1
+
+while True:
+    if button_a.is_pressed():
+        cookie_number = cookie_number + cookie_increase
+        display.scroll(cookie_number)
+
+    if cookie_number > 14:
+        cookie_increase += 1
+        cookie_number -= 15
+        display.scroll("Upgraded!")
+        sleep(200) 
+    else:
+        display.scroll("Too Poor!")
+        sleep(200)  
